781252ae46d6bb1cc936b5a6c1c8e4ae81d3dda1
Leandri Beavis <leandrikuyk@gmail.com>
2022-07-31 19:13:52 -0400

    create integrate thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    addThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+
+// Set up GET all and POST at /api/pizzas
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+module.exports = router; 
